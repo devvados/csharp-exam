68850f3425df9642291b8aa7e5d3678ae9b6291c
Наталья <@DESKTOP-2TA5QJ9>
2017-08-07 12:39:38 +0300

    Add project files.

---
+++ ShapesTask/Figure.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesTask
+{
+    abstract class Figure
+    {
+        public abstract double Square();
+    }
+
+    class Rectangle : Figure
+    {
+        double _length, _width;
+
+        public double Length
+        {
+            get
+            {
+                return _length;
+            }
+
+            set
+            {
+                _length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+
+            set
+            {
+                _width = value;
+            }
+        }
+
+        public Rectangle()
+        {
+            Random rand = new Random();
+            Length = rand.Next(3, 10);
+            Width = rand.Next(5, 15);
+        }
+
+        public override double Square()
+        {
+            return Length * Width;
+        }
+    }
+
+    class Triangle : Figure
+    {
+        Random rand;
+        double _a, _b, _c;
+
+        public double A
+        {
+            get
+            {
+                return _a;
+            }
+
+            set
+            {
+                _a = value;
+            }
+        }
+
+        public double B
+        {
+            get
+            {
+                return _b;
+            }
+
+            set
+            {
+                _b = value;
+            }
+        }
+
+        public double C
+        {
+            get
+            {
+                return _c;
+            }
+
+            set
+            {
+                _c = value;
+            }
+        }
+
+        public Triangle()
+        {
+            rand = new Random();
+            do
+            {
+                RandomizeDimensions(rand);
+            }
+            while (!CheckTriangle());
+        }
+
+        public void RandomizeDimensions(Random r)
+        {
+            A = r.Next(3, 10);
+            B = r.Next(3, 10);
+            C = r.Next(3, 10);
+        }
+
+        public bool CheckTriangle()
+        {
+            bool exists = false;
+            if ((A + B > C) && (A + C > B) && (B + C > A))
+                exists = true;
+            return exists;
+        }
+
+        public double FindPerimeter()
+        {
+            return (A + B + C) / 2;
+        }
+
+        public override double Square()
+        {
+            double p = this.FindPerimeter();
+
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
---
+++ ShapesTask/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesTask
+{
+    class Program
+    {
+        static List<Figure> figures = new List<Figure>();
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Random rand = new Random();
+             
+            Console.Write("Выберите количество генерируемых фигур: ");
+            Int32.TryParse(Console.ReadLine(), out count);
+
+            CreateRandomShape(count, rand);
+
+            foreach (var f in figures)
+            {
+                Console.WriteLine(String.Format("Тип: {0}, Площадь: {1}", f.GetType().ToString().Split('.')[1], f.Square()));
+            }
+        }
+
+        public static void CreateRandomShape(int count, Random r)
+        {
+            List<string> figureTypes = new List<string> { "Rectangle", "Triangle" };
+
+            for (int i = 0; i < count; i++)
+            {
+                var rand = r.Next(0, figureTypes.Count);
+                switch (rand)
+                {
+                    case 0:
+                        figures.Add(new Rectangle());
+                        break;
+                    case 1:
+                        figures.Add(new Triangle());
+                        break;
+                }
+            }
+        }
+    }
+}
